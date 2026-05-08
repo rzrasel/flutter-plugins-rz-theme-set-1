c7c2003817892f9f21eda9471a4bb651a3d3af1e
Rz Rasel <emailrzrasel@gmail.com>
2026-05-08 22:44:44 +0600

    Initial Commit

--- lib/src/themes/size/rz_size.dart
+++ lib/src/themes/size/rz_size.dart
@@ -32,6 +32,7 @@ class RzSize {
 
   // Default spacing between sections
   static const double defaultSpace = 24.0;
+  final double defaultSpaceGet = 24.0;
   static const double spaceBetweenItems = 16.0;
   static const double spaceBetweenSections = 32.0;
 
